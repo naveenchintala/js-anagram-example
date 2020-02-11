e6607c5acd2b3eaec18e465efbfc317a9a3195be
naveenchintala <naveenkchintala@gmail.com>
2020-02-11 22:00:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-anagram-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-anagram-example)
---
+++ index.js
@@ -0,0 +1,16 @@
+function getAnagrams(arr) {
+  let map = new Map();
+  for (let word of arr) {
+    // split the word by letters, sort them and join back
+    let sorted = word
+      .toLowerCase()
+      .split("")
+      .sort()
+      .join(""); // (*)
+    map.set(sorted, word);
+  }
+  return Array.from(map.values());
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+console.log(getAnagrams(arr));
